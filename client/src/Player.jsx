40bdd1e507135a8937f10a43ce1f881e37685cc5
GuessWhatBBQ <saminslm@gmail.com>
2022-10-14 09:29:54 +0600

    feat: video streaming

---
+++ client/src/Player.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Player, Ui } from "@vime/react";
+
+function Player() {
+  return (
+    <Player>
+      {/* Provider component is placed here. */}
+
+      <Ui>{/* UI components are placed here. */}</Ui>
+    </Player>
+  );
+}
