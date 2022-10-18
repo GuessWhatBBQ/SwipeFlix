cb698cea452c7d0b54f1c82b9432d5bd51279d0d
KabirBhae <arafatkabirkhan22@gmail.com>
2022-10-19 02:24:59 +0600

    no major changes. fps showing on screen

--- client/src/Mediapipe/VolumeControls.py
+++ client/src/Mediapipe/VolumeControls.py
@@ -2,6 +2,7 @@ import cv2
 import mediapipe as mp
 import time
 
+
 #####
 cwidth, cheight = 640, 480
 #####
@@ -9,6 +10,7 @@ cap = cv2.VideoCapture(0)
 cap.set(3, cwidth)
 cap.set(4, cheight)
 
+ptime = 0
 
 detector = htm.HandDetector()
 
@@ -17,5 +19,12 @@ while True:
     success, img = cap.read()
     img = cv2.flip(img, 1)
 
+
+    #getting fps
+    ctime = time.time()
+    frame_rate = 1/(ctime - ptime)
+    ptime = ctime
+
+    cv2.putText(img, f'FPS: {int(frame_rate)}', (10, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0), 1)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
