66a45a24274dab076a2406461c55939efbaf2536
KabirBhae <arafatkabirkhan22@gmail.com>
2022-10-19 10:46:24 +0600

    added volume increase decrease functionality

--- client/src/Mediapipe/VolumeControls.py
+++ client/src/Mediapipe/VolumeControls.py
@@ -1,10 +1,14 @@
 import cv2
 import mediapipe as mp
 import time
+import numpy as np
 import math
 
 import HandTrackingModule as htm
 
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 #####
 cwidth, cheight = 640, 480
@@ -16,6 +20,18 @@ cap.set(4, cheight)
 ptime = 0
 
 detector = htm.HandDetector()
+
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+# volume.GetMute()
+# volume.GetMasterVolumeLevel()
+minVolume, maxVolume = volume.GetVolumeRange()[0], volume.GetVolumeRange()[1]
+
+
+
 
 while True:
     # getting the frame
@@ -30,8 +46,13 @@ while True:
         cv2.circle(img, (cx4, cy4), 5, (255, 0, 0), cv2.FILLED)
         cv2.circle(img, (cx8, cy8), 5, (255, 0, 0), cv2.FILLED)
 
+        #length 25-180
         length = math.hypot(cx8-cx4, cy8-cy4)
-        print(length)
+        vol = np.interp(length, [20, 170], [minVolume, maxVolume])
+        #third party code        
+        volume.SetMasterVolumeLevel(vol, None)
+        # print(length, vol)
+
         if(length <30):
             cv2.circle(img, (cx4, cy4), 5, (0, 255, 0), cv2.FILLED)
             cv2.circle(img, (cx8, cy8), 5, (0, 255, 0), cv2.FILLED)
