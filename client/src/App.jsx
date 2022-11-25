4390bc5475c033389f135236c3c0d0a81b8fec94
agninoor <otulonio.noor@gmail.com>
2022-11-25 10:06:48 +0600

    rrd added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,26 @@
-import "./app.scss";
+import "./app.scss"
 import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 import Login from "./pages/login/Login";
 import {
-  BrowserRouter as Router,
-  Switch,
+  BrowserRouter,
+  Routes,
   Route,
-  Redirect,
+  Link,
 } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "./authContext/AuthContext";
 
 const App = () => {
-  const { user } = useContext(AuthContext);
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          {user ? <Home /> : <Redirect to="/register" />}
-        </Route>
-        <Route path="/register">
-          {!user ? <Register /> : <Redirect to="/" />}
-        </Route>
-        <Route path="/login">{!user ? <Login /> : <Redirect to="/" />}</Route>
-        {user && (
-          <>
-            <Route path="/movies">
-              <Home type="movie" />
-            </Route>
-            <Route path="/series">
-              <Home type="series" />
-            </Route>
-            <Route path="/watch">
-              <Watch />
-            </Route>
-          </>
-        )}
-      </Switch>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/watch" element={<Watch/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+
+      </Routes>
+    </BrowserRouter>
   );
 };
 
