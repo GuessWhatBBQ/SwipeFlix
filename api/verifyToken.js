b1b50bdb4d66b770a9ccff27d65d852682916e57
agninoor <otulonio.noor@gmail.com>
2022-12-30 01:04:56 +0600

    verifyToken modified

--- api/verifyToken.js
+++ api/verifyToken.js
@@ -5,7 +5,7 @@ function verify(req, res, next) {
   if (authHeader) {
     const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
-      if (err) res.status(403).json("Token is not valid");
+      if (err) return res.status(403).json("Token is not valid");
       req.user = user;
       next();
     });
